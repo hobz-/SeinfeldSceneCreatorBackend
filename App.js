7f6b1954c119add4c7a8207edf560224c21d4b81
hobz- <josh.n.hogg@gmail.com>
2017-12-04 00:36:10 -0700

    Redis fix x2

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-var gifQueue = new Queue('gif combiner', redis://h:ec2-34-239-85-93.compute-1.amazonaws.com:26409);
+var gifQueue = new Queue('gif combiner', process.env.REDIS_URL);
 
 gifQueue.process(function(job, done) {
   console.log("queueing");
